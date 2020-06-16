8320dabfef6e439515d79d0f3804dfb18eb4d3fc
ogarmela <52357639+ogarmela@users.noreply.github.com>
2020-06-15 21:05:38 -0700

    Add files via upload

---
+++ fullmethod.java
@@ -0,0 +1,21 @@
+package muno;
+
+public class fullmethod {
+	public boolean isFull() {
+	    return isFull(overallRoot);
+	}
+
+	public boolean isFull(IntTreeNode root) {
+	    if (root == null) {
+	        return true;
+	    } else if (root.left == null && root.right != null) {
+	        return false;
+	    } else if (root.right == null && root.left != null) {
+	        return false;
+	    } else {
+	        return isFull(root.left) && isFull(root.right);
+	    }
+	}
+
+
+}
